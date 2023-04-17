a8796e5efe03fe26c2ffb665ac13524b6e89bda0
FreelancerShijan <freelancershijan@gmail.com>
2023-04-17 22:36:46 +0600

    finished

---
+++ index.js
@@ -0,0 +1,10 @@
+// Assignment
+
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        let product = num * i;
+        console.log(num + "x" + i + "=" + product);
+    }
+}
+
+multiplicationTable(5);
